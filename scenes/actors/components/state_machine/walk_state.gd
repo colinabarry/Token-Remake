6f378482a246638f7dde7a776ca1ba72d7c74d4c
Colin Barry <colin.andrew.barry@gmail.com>
2023-05-09 14:37:17 -0400

    cleanup

--- scenes/actors/components/state_machine/walk_state.gd
+++ scenes/actors/components/state_machine/walk_state.gd
@@ -9,12 +9,6 @@ func input(_event: InputEvent) -> int:
 
 
 func process(_delta: float) -> int:
-	# var input_dir_x := (
-	# 	-Input.get_action_strength("move_left") + Input.get_action_strength("move_right")
-	# )
-	# player.velocity.x = input_dir_x * player.walk_speed
-	# player.velocity.y += player.gravity
-
 	if !player.is_on_floor():
 		return State.FALL
 	if input_dir_x == 0:
