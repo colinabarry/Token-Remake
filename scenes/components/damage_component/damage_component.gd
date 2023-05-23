0e762b1f219dd32ce13e7d9c802073367b29a0da
Colin Barry <colin.andrew.barry@gmail.com>
2023-05-23 16:31:27 -0400

    health and damage component work

--- scenes/components/damage_component/damage_component.gd
+++ scenes/components/damage_component/damage_component.gd
@@ -1,3 +1,12 @@
+@icon("res://assets/icons/icon_damage_component.png")
 class_name DamageComponent extends Area2D
 
 @export var damage := 1
+
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass
--- scenes/components/health_component/health_component.gd
+++ scenes/components/health_component/health_component.gd
@@ -1,8 +1,13 @@
+@icon("res://assets/icons/icon_health_component.png")
 class_name HealthComponent extends Area2D
 
 @export var base_health := 1
 
 var health := base_health
+
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
 
 
 func _take_damage(damage_component: DamageComponent) -> void:
