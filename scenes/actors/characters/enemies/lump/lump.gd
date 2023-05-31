69485f68d009d8c06beffdae5e1a830220b6096f
Colin Barry <colin.andrew.barry@gmail.com>
2023-05-31 01:39:22 -0400

    enemy can hop

--- scenes/actors/characters/enemies/lump/lump.gd
+++ scenes/actors/characters/enemies/lump/lump.gd
@@ -1,8 +1,9 @@
-extends Sprite2D
+extends CharacterBody2D
 
 enum DIR { RIGHT = 1, LEFT = -1 }
 
 @export var animation_player: AnimationPlayer
+@export var is_hopper := false
 @export var patrol_width := 16.0
 @export var patrol_speed := 10.0
 @export var start_dir := DIR.RIGHT
@@ -12,6 +13,7 @@ var is_alive := true
 
 @onready var health := $HealthComponent as HealthComponent
 @onready var damage := $DamageComponent as DamageComponent
+@onready var hop_detector := $HopDetector as Area2D
 
 @onready var spawn_position := position
 
@@ -22,23 +24,47 @@ func _ready() -> void:
 
 	if patrol_width > 0:
 		animation_player.play("walk")
-	# else:
-	# 	animation_player.play("RESET")
 
 
 func _process(delta: float) -> void:
-	if is_alive:
-		if patrol_width > 0:
-			position.x = move_toward(
-				position.x, spawn_position.x + patrol_width * dir, patrol_speed * delta
-			)
-			if abs(position.x - spawn_position.x) == patrol_width:
-				dir *= -1
+	# if is_hopper:
+	velocity.y += 9.8
+	move_and_slide()
+
+	if not is_alive:
+		velocity.x = 0
+		return
+
+	if patrol_width > 0:
+		position.x = move_toward(
+			position.x, spawn_position.x + patrol_width * dir, patrol_speed * delta
+		)
+		if abs(position.x - spawn_position.x) == patrol_width:
+			dir *= -1
 
 
 func die() -> void:
-	if is_alive:
-		is_alive = false
-		damage.queue_free()
-		health.queue_free()
-		animation_player.play("die")
+	if not is_alive:
+		return
+
+	is_alive = false
+	damage.queue_free()
+	health.queue_free()
+	hop_detector.queue_free()
+	animation_player.play("die")
+
+
+func _hop() -> void:
+	print_debug("hoppy")
+	velocity.y = -250.0
+
+
+func _on_hop_detector_body_entered(body: Node2D) -> void:
+	if not is_alive:
+		return
+
+	var player := body as Player
+	if not player:
+		return
+
+	_hop()
