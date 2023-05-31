66c5e49936aa57d7a153b55caf8ffa869858272e
Colin Barry <colin.andrew.barry@gmail.com>
2023-05-31 02:13:31 -0400

    add and test spring

---
+++ scenes/actors/spring/spring.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# controlled in animation
+var can_activate := true:
+	set(val):
+		can_activate = val
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not can_activate:
+		return
+
+	if body.has_method("move_and_slide"):
+		body.velocity.y = -250
+		$AnimationPlayer.play("activate")
