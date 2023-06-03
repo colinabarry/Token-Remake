5edd67663ce50734385646b811cd58e6d689126f
Colin Barry <colin.andrew.barry@gmail.com>
2023-06-02 23:21:57 -0400

    scene manager is closer to being real

---
+++ scenes/levels/scene_manager/scene_manager.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+@export var animation_player: AnimationPlayer
+@export var player: Player
+@export var post_fx_layers: Array[CanvasLayer]
+@export var levels: PackedStringArray
