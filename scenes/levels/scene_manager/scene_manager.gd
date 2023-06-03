9d0de87e8ba4cd0b658b86c50a25cfceef094cde
Colin Barry <colin.andrew.barry@gmail.com>
2023-06-03 18:25:08 -0400

    scene manager is so much closer to being real

--- scenes/levels/scene_manager/scene_manager.gd
+++ scenes/levels/scene_manager/scene_manager.gd
@@ -12,6 +12,8 @@ var next_level: Level
 
 
 func _ready() -> void:
+	player.died.connect(_on_player_died)
+
 	if levels.size() == 0:
 		return
 
@@ -19,3 +21,21 @@ func _ready() -> void:
 		current_level = load(levels[1]).instantiate()
 
 	level_layer.add_child(current_level)
+
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("level_reset"):
+		_change_scene(current_level.level_index)
+
+
+func _change_scene(to_level_index: int) -> void:
+	player.position = player.spawn_location
+	player.velocity = Vector2.ZERO
+	var new_level: Level = load(levels[to_level_index]).instantiate()
+	current_level.queue_free()
+	current_level = new_level
+	level_layer.add_child(current_level)
+
+
+func _on_player_died() -> void:
+	_change_scene(current_level.level_index)
