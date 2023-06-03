521ae09467eb0d7c3f2d291d955b70517533fcbf
Colin Barry <colin.andrew.barry@gmail.com>
2023-06-03 18:24:50 -0400

    make walk animation speed scale with speed

--- scenes/components/state_machine/walk_state.gd
+++ scenes/components/state_machine/walk_state.gd
@@ -5,6 +5,14 @@ func _init() -> void:
 	resource_name = "WalkState"
 
 
+func exit() -> void:
+	player.anim_player.speed_scale = 1
+
+
 func physics_process(_delta: float) -> void:
 	player.move()
 	player.fall()
+
+	player.anim_player.speed_scale = remap(
+		abs(player.velocity.x), 0, player.max_walk_speed, 0.25, 1
+	)
